3fa655745da916139facd4783ac0e81ef6e22702
Victoria Buchanan <victoriabuchanan@pursuit.org>
2019-02-08 20:45:20 -0500

    list created

--- src/App.js
+++ src/App.js
@@ -2,15 +2,24 @@ import React, { Component } from 'react';
 
 const inProgess = [
   {
-    title: "Do Codewars Challege ",
-    tags: [{type:Coding, color: blue},{type: daily, color: red }]
+    title: "Redo Unit 2 Exam",
+    tags: [{type:Coding, color: blue}]
   },
   {
-
+    title: "Read JavaScript & Jquery Textbook",
+    tags: [{type: Coding, color: blue}, {type: Reading, color: green}]
   },
   {
-
+    title: "Design New Logo for //codeBreak",
+    tags: [{type: Podcast, color: orange}, {type: Marketing, color:purple}]
   },
+]
+
+const completed = [
+  {
+    title: "Trello Remake",
+    tags:[{type:Coding, color: blue}]
+  }
 ]
 
 
---
+++ src/container/list.js
@@ -0,0 +1,15 @@
+import react from 'react';
+
+class List extends React.component {
+
+    constructor(props){
+        super (props);
+
+        this.state ={
+            title: "In Progress"
+        }
+    }
+
+}
+
+export default List;
