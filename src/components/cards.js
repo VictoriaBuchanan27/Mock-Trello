c5707a68768f56f697ddb210322038f6d9cdb229
Victoria Buchanan <victoriabuchanan@pursuit.org>
2019-03-05 23:45:17 -0500

    changes made to the card component

--- src/components/cards.js
+++ src/components/cards.js
@@ -12,24 +12,25 @@ const Cards = (props)=>{
         {
             props.tags.map((e,i)=>{
                 return <Tag color={e.color} type={e.type} key={i}/>
+                
             })
         }
         </div>
         </div>
     );
-    const EmptyCards=props=>{
-        return(
-            <div className='empty'>
-            <span className='empty-title'>{props.empty.title}</span>
-            <div className='empty-card-holder'>
-            {
-            props.tags.map((e,i)=>{
-                return <Tag color={e.color} type={e.type} key={i}/>
-            })
-        }
-            </div>
-            </div>
-        )
+    // const EmptyCards=props=>{
+    //     return(
+    //         <div className='empty'>
+    //         <span className='empty-title'>{props.empty.title}</span>
+    //         <div className='empty-card-holder'>
+    //         {
+    //         props.tags.map((e,i)=>{
+    //             return <Tag color={e.color} type={e.type} key={i}/>
+    //         })
+    //     }
+    //         </div>
+    //         </div>
+    //     )
     
 }
 export default Cards;
